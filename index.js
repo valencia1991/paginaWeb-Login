a6828212daa0d7e7a61fb22cbb51d63168872953
er.and1991@hotmail.com <er.and1991@hotmail.com>
2022-12-10 11:20:10 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// function obtenerLogin(){
+//     let emailLogin = document.getElementById('emailLogin').value;
+//     let   passLoging = document.getElementById('passLogin').value;
+//         console.log(emailLogin,passLoging)
+// }
+function ingresar(){
+  if (document.form1.txt.value=="admin@gmail.com" && document.form1.num.value=="1234"){window.location="index2.html"}
+  else {alert("Error en Usuario o Contraseña. Intenta de nuevo.")}
+  }
+  document.oncontextmenu=new Function("return false");
+
+  
